8bd9fbef1afcd1a840ac244a538327f0c30b6f75
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-24 11:28:24 -0500

    added apache

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,9 @@
 node {
     stage("Say Hello"){
-        properties([parameters([string(defaultValue: '34.253.214.183', description: '''Dev: 34.253.214.183 Qa: 34.254.184.67
-Prod: 34.253.225.101''', name: 'Remote_instance', trim: true)])])
+        properties([parameters([string(defaultValue: '34.253.214.183', description: '''Dev: 34.253.214.183 Qa: 34.254.184.67 Prod: 34.253.225.101''', name: 'Remote_instance', trim: true)])])
         git 'https://github.com/dilfuza97/jenkins_html.git'
+    }
+    stage("Install Apache"){
+        sh "ssh ec2-user@${Remote_instance}  sudo yum install httpd -y"
     }
 }
