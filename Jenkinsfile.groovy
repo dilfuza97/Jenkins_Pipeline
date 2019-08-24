54808a867c7500327babe4ecc7c6f79e16ee6f74
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-24 11:24:32 -0500

    added environment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,7 @@
 node {
     stage("Say Hello"){
+        properties([parameters([string(defaultValue: '34.253.214.183', description: '''Dev: 34.253.214.183 Qa: 34.254.184.67
+Prod: 34.253.225.101''', name: 'Remote_instance', trim: true)])])
         git 'https://github.com/dilfuza97/jenkins_html.git'
     }
 }
