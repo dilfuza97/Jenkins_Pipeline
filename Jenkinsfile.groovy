9e69588686af7bdc3517a2ed752a4aa51e948a95
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-24 11:09:19 -0500

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
-ode {
+node {
     stage("Say Hello"){
         properties([parameters([string(defaultValue: 'Dilfuza', description: 'Paste your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * * ')])])
         sh "echo Hello ${NAME}"
